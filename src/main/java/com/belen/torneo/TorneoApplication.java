8f41abdef06b6b4280757863e2d88f43ffb5b8a8
Jorge Bernal Delgado <jorge-jbd@hotmail.com>
2023-11-27 20:54:45 -0500

    se deshabilita la seguridad

--- src/main/java/com/belen/torneo/TorneoApplication.java
+++ src/main/java/com/belen/torneo/TorneoApplication.java
@@ -2,11 +2,12 @@ package com.belen.torneo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 @EnableSwagger2
 public class TorneoApplication extends SpringBootServletInitializer {
 

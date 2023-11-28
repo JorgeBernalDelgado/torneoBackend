446545c1bb0fc19b1c5741a3a9e89829e84d61c2
Jorge Bernal Delgado <jorge-jbd@hotmail.com>
2023-11-27 23:39:46 -0500

    configura cors en config

--- src/main/java/com/belen/torneo/TorneoApplication.java
+++ src/main/java/com/belen/torneo/TorneoApplication.java
@@ -2,12 +2,11 @@ package com.belen.torneo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+@SpringBootApplication
 @EnableSwagger2
 public class TorneoApplication extends SpringBootServletInitializer {
 
